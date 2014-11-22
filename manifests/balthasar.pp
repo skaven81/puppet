9323d2d77fa45d9485a5b713e27c89cfa3894ae9
Paul Krizak <paul.krizak@gmail.com>
2014-11-22 13:08:02 -0800

    use account instead

--- manifests/balthasar.pp
+++ manifests/balthasar.pp
@@ -2,4 +2,15 @@
 # the AWS instance that hosts a
 # simple web server.
 
-class {'ec2_user':}
+account {'ec2-user':
+    ensure  => 'present',
+    uid     => 1000,
+    shell   => '/bin/bash',
+    manage_home => true,
+    create_group => true,
+    groups  => [ 'adm', 'wheel', 'systemd-journal' ]
+    ssh_key => file('pubkeys/token'),
+    comment => 'AWS-provided login',
+}
+
+
