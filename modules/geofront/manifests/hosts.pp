99c1ed6298676a2ed1c5bcb93dcba287d3327b22
Paul Krizak <paul.krizak@gmail.com>
2015-03-14 11:01:50 -0700

    Add wap

--- modules/geofront/manifests/hosts.pp
+++ modules/geofront/manifests/hosts.pp
@@ -1,4 +1,9 @@
 class geofront::hosts {
+    host { 'wap':
+        ensure => present,
+        host_aliases => 'wap.attlocal.net',
+        ip => '192.168.1.251',
+    }
     host { 'geofront':
         ensure => present,
         host_aliases => 'geofront.attlocal.net',
