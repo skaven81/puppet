d59befa0903192c3b74ca15bdb1853ac9bde6173
Paul Krizak <paul.krizak@gmail.com>
2017-12-09 21:22:31 -0800

    Update SSL for letsencrypt

--- manifests/balthasar.pp
+++ manifests/balthasar.pp
@@ -151,4 +151,12 @@ Apache::Vhost <| title == 'default' |> {
 Apache::Vhost <| title == 'default-ssl' |> {
     docroot        => '/var/www/html',
     directoryindex => 'index.html index.htm index.php',
+    ssl_cert       => '/etc/letsencrypt/live/balthasar.viewdns.net/cert.pem',
+    ssl_key        => '/etc/letsencrypt/live/balthasar.viewdns.net/privkey.pem',
+    ssl_chain      => '/etc/letsencrypt/live/balthasar.viewdns.net/chain.pem',
+    ssl_protocol   => [ 'all', '-SSLv2', '-SSLv3' ],
+    ssl_cipher     => 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA',
+    ssl_honorcipherorder => 'on',
+    ssl_options    => [ '+StrictRequire' ],
+    serveraliases  => [ 'balthasar.viewdns.net', 'balthasar.logastro.com' ],
 }
