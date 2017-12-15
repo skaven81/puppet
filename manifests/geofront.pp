9fe28a447c0a69715e35a8ba14125afeb1f6d480
Paul Krizak <paul.krizak@gmail.com>
2017-12-14 22:09:58 -0800

    Set async

--- manifests/geofront.pp
+++ manifests/geofront.pp
@@ -448,7 +448,7 @@ file { '/etc/exports':
     owner   => 'root',
     group   => 'root',
     mode    => '0444',
-    content => '/raid 192.168.86.0/24(rw,sync,root_squash,no_subtree_check)
+    content => '/raid 192.168.86.0/24(rw,async,root_squash,no_subtree_check)
 ',
     notify  => Exec['exportfs'],
 } ->
