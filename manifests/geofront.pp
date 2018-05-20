38b79ff431778314f83ae92bed6897acd23abd56
Paul Krizak <paul.krizak@gmail.com>
2018-05-19 17:07:49 -0700

    Missed comma

--- manifests/geofront.pp
+++ manifests/geofront.pp
@@ -69,7 +69,7 @@ cron { 'puppet':
 cron { 'hpdm1z-kerberos':
     ensure  => 'present',
     user    => 'root',
-    command => 'curl --max-time 1 --fail -s http://hpdm1z:501/login >/dev/null || echo "kerberos:501 is not responding"'
+    command => 'curl --max-time 1 --fail -s http://hpdm1z:501/login >/dev/null || echo "kerberos:501 is not responding"',
     minute  => 0,
 }
 
