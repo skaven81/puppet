efd8c3ac3c33fceec1bfa527acb8e3212d9d6dba
Paul Krizak <paul.krizak@gmail.com>
2019-01-12 11:34:09 -0800

    Don't restart every night

--- manifests/geofront.pp
+++ manifests/geofront.pp
@@ -80,15 +80,6 @@ cron { 'kerberos-cleanup-jpg':
     minute  => 0,
     hour    => 2,
 }
-cron { 'kerberos-weekly-restart':
-    ensure  => 'present',
-    user    => 'root',
-    command => 'systemctl restart kerberos',
-    minute  => 0,
-    hour    => 1,
-    weekday => 1, # monday
-}
-
 
 file { '/etc/cron.weekly/astro_photo_cleanup':
     ensure  => 'present',
